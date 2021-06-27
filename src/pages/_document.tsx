75f03984e07713517f09e4434ab8baa322989dbd
Tom Digby <tom@atomlearning.co.uk>
2021-06-27 16:40:51 +0100

    fix: unnecessary preconnect

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,6 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Space+Grotesk:wght@700&display=swap"
             rel="stylesheet"
